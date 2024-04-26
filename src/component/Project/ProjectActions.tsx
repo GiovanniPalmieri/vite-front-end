62fa2f5a6c29bddef0adcfab10c9c7763f24ea5f
GiovanniPalmieri <48354624+GiovanniPalmieri@users.noreply.github.com>
2024-04-26 16:44:47 +0200

    added action to set project for synchronizing with database

--- src/component/Project/ProjectActions.tsx
+++ src/component/Project/ProjectActions.tsx
@@ -3,3 +3,4 @@ import { ProjectEntity } from "../../api/ApiEntities";
 export type ProjectAction =
     | { type: 'CREATE_PROJECT'; project: ProjectEntity }
     | { type: 'DELETE_PROJECT'; project: ProjectEntity }
+    | {type: 'SET_PROJECTS'; projects: ProjectEntity[]}
